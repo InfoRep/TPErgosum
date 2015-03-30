3b04690b1231a20b12a198c5e5f853af7eb860d8
QuentinV <quentvht@hotmail.com>
2015-03-30 10:52:32 +0200

    Init MultiController Init class persistance Init class Metier

---
+++ src/main/java/com/epul/ergosum/metier/GestionErgosum.java
@@ -0,0 +1,93 @@
+package com.epul.ergosum.metier;
+
+import java.util.HashMap;
+
+import com.epul.ergosum.meserreurs.MonException;
+
+public class GestionErgosum {
+
+	public Object listerTousLesJouets(int categorieCode, int trancheCode) throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object listerToutesLesCategories() throws MonException  {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object listerToutesLesTranches() throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object listerTousLesCatalogues() throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Jouet rechercherJouet(String id) throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Categorie rechercherCategorie(String parameter) throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Trancheage rechercherTrancheage(String parameter) throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void modifier(Jouet unJouet) throws MonException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Catalogue rechercherCatalogue(String parameter) throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void modifierCatalogue(Catalogue leCatalogue) throws MonException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Ajouter un jouet
+	 * @param unJouet
+	 * @throws MonException
+	 */
+	public void ajouter(Jouet unJouet) throws MonException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object listerTousLesJouets() throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Effacer des jouets
+	 * @param ids
+	 */
+	public void effacer(String[] ids) throws MonException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object listerCatalogueQuantites(int parseInt, int parseInt2) throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public HashMap<Categorie, Integer> rechercherDictionnaire(String parameter) throws MonException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
